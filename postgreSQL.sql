3eff907aaef988182745e1d1aaec6f562c7ad35e
abha10 <ghatkar.abhaya@gmail.com>
2019-07-05 15:03:29 +0530

    Update postgreSQL.sql

--- postgreSQL.sql
+++ postgreSQL.sql
@@ -1,3 +1,11 @@
-#Provide READ Only access
+#Create User with Password
+
+CREATE USER <USERNAME> WITH PASSWORD '<Password>';
+
+#Provide READ Only access on ALL Tables
 
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO <username>;
+
+#Provide READ only access on a specific Table
+
+GRANT SELECT ON <TABLENAME> TO <USERNAME>;
